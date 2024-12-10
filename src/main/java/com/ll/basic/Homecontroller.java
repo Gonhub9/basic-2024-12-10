b9b36a0b72a1b1d3abf2b7ed8f1b9c25dacdfbe4
Gonhub9 <ho_gok@naver.com>
2024-12-10 14:11:18 +0900

    article json2

--- src/main/java/com/ll/basic/Homecontroller.java
+++ src/main/java/com/ll/basic/Homecontroller.java
@@ -18,49 +18,84 @@ public class Homecontroller {
 
     @GetMapping("/about")
     @ResponseBody
-    public String showAbout() { return "저는 홍길동입니다."; }
+    public String showAbout() {
+        return "저는 홍길동입니다.";
+    }
 
     @GetMapping("/ageUp")
     @ResponseBody
-    public int showAge() { return age++; }
+    public int showAge() {
+        return age++;
+    }
 
     @GetMapping("/long")
     @ResponseBody
-    public long getLong() { return 1000000000; }
+    public long getLong() {
+        return 1000000000;
+    }
 
     @GetMapping("/boolean")
     @ResponseBody
-    public boolean getBoolean() { return true; }
+    public boolean getBoolean() {
+        return true;
+    }
 
     @GetMapping("/byte")
     @ResponseBody
-    public byte getByte() { return 127; }
+    public byte getByte() {
+        return 127;
+    }
 
     @GetMapping("/short")
     @ResponseBody
-    public short getShort() { return 32000; }
+    public short getShort() {
+        return 32000;
+    }
 
     @GetMapping("/char")
     @ResponseBody
-    public char getChar() { return '꼭'; }
+    public char getChar() {
+        return '꼭';
+    }
 
     @GetMapping("/float")
     @ResponseBody
-    public float getFloat() { return 3.14f; }
+    public float getFloat() {
+        return 3.14f;
+    }
 
     @GetMapping("/double")
     @ResponseBody
-    public double getDouble() { return 3.141592; }
+    public double getDouble() {
+        return 3.141592;
+    }
 
     @GetMapping("/list")
     @ResponseBody
-    public List<String> getList() { return List.of("a", "b", "c"); }
+    public List<String> getList() {
+        return List.of("a", "b", "c");
+    }
 
     @GetMapping("/array")
     @ResponseBody
-    public List<String> getArray() { return List.of("a", "b", "c"); }
+    public List<String> getArray() {
+        return List.of("a", "b", "c");
+    }
 
     @GetMapping("/map")
     @ResponseBody
-    public Map<String, String> getMap() { return Map.of("name :", "Paul", "hobby", "reading"); }
+    public Map<String, String> getMap() {
+        return Map.of("name ", "Paul", "hobby ", "reading");
+    }
+
+    @GetMapping("/article")
+    @ResponseBody
+    public Article getAticle() {
+        return new Article();
+    }
+}
+
+Class Article {
+    private long id;
+
 }
