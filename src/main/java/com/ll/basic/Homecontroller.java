884acd0a902e45ad08428e81af8ef81b0e4d9932
Gonhub9 <ho_gok@naver.com>
2024-12-10 12:22:53 +0900

    Create basic

---
+++ src/main/java/com/ll/basic/Homecontroller.java
@@ -0,0 +1,12 @@
+package com.ll.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Homecontroller {
+    @GetMapping("/")
+    @ResponseBody
+    public String showMain() { return "안녕하세요."; }
+}
