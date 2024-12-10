6802e6b1755fea8b7d418702a396aa2f3f22a8ba
Gonhub9 <ho_gok@naver.com>
2024-12-10 12:54:12 +0900

    get

--- src/main/java/com/ll/basic/Homecontroller.java
+++ src/main/java/com/ll/basic/Homecontroller.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+import java.util.Map;
+
 // @Controller 어노테이션을 붙여주면 이 스프링부트가
 @Controller
 public class Homecontroller {
@@ -20,4 +23,44 @@ public class Homecontroller {
     @GetMapping("/ageUp")
     @ResponseBody
     public int showAge() { return age++; }
+
+    @GetMapping("/long")
+    @ResponseBody
+    public long getLong() { return 1000000000; }
+
+    @GetMapping("/boolean")
+    @ResponseBody
+    public boolean getBoolean() { return true; }
+
+    @GetMapping("/byte")
+    @ResponseBody
+    public byte getByte() { return 127; }
+
+    @GetMapping("/short")
+    @ResponseBody
+    public short getShort() { return 32000; }
+
+    @GetMapping("/char")
+    @ResponseBody
+    public char getChar() { return '꼭'; }
+
+    @GetMapping("/float")
+    @ResponseBody
+    public float getFloat() { return 3.14f; }
+
+    @GetMapping("/double")
+    @ResponseBody
+    public double getDouble() { return 3.141592; }
+
+    @GetMapping("/list")
+    @ResponseBody
+    public List<String> getList() { return List.of("a", "b", "c"); }
+
+    @GetMapping("/array")
+    @ResponseBody
+    public List<String> getArray() { return List.of("a", "b", "c"); }
+
+    @GetMapping("/map")
+    @ResponseBody
+    public Map<String, String> getMap() { return Map.of("name :", "Paul", "hobby", "reading"); }
 }
