2c2922652db3b1e19d8017eab278684d02ec33a4
Gonhub9 <ho_gok@naver.com>
2024-12-11 14:09:33 +0900

    Required

--- src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -2,6 +2,7 @@ package com.ll.basic.domain.wiseSaying.wiseSaying.controller;
 
 import com.ll.basic.domain.wiseSaying.wiseSaying.entity.WiseSaying;
 import com.ll.basic.domain.wiseSaying.wiseSaying.service.WiseSayingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,17 +10,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
+@ResponseBody // 여기에 붙이면 모든 메서드에 적용
+@RequiredArgsConstructor // 생성자를 자동으로 만들어줌, final 붙은 필드를 자동으로 초기화해주는 어노테이션
+
 public class WiseSayingController {
     private final WiseSayingService wiseSayingService;
 
-    public WiseSayingController () {
-        this.wiseSayingService = new WiseSayingService();
-
-    }
+    // @Autowired로 필요한 것이 있으면 () 안에 적어라. 근데 생략 가능
+    // @Autowired
 
     @GetMapping("/wiseSayings")
-    @ResponseBody
     public List<WiseSaying> getItems() {
         return wiseSayingService.findAll();
     }
+
 }
--- src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/service/WiseSayingService.java
+++ src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/service/WiseSayingService.java
@@ -1,11 +1,15 @@
 package com.ll.basic.domain.wiseSaying.wiseSaying.service;
 
 import com.ll.basic.domain.wiseSaying.wiseSaying.entity.WiseSaying;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+// Component를 붙이면 new를 할 필요가 없이 스프링부트가 알아서 만들어준다.
+// Service와 Component의 차이는 '가독성'이다. 하지만 서비스에 해당하는 클래스는 Service를 사용하는 것이 좋다.
+@Service
 public class WiseSayingService {
     private final List<WiseSaying> wiseSayings;
     private long lastId;
