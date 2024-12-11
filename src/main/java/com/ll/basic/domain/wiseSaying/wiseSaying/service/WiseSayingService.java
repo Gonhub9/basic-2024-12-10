ae48689efddcf54a64f6d783819e60e5459fff71
Gonhub9 <ho_gok@naver.com>
2024-12-11 17:02:19 +0900

    repository

--- src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/service/WiseSayingService.java
+++ src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/service/WiseSayingService.java
@@ -1,73 +1,39 @@
 package com.ll.basic.domain.wiseSaying.wiseSaying.service;
 
 import com.ll.basic.domain.wiseSaying.wiseSaying.entity.WiseSaying;
+import com.ll.basic.domain.wiseSaying.wiseSaying.repository.WiseSayingRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 // Component를 붙이면 new를 할 필요가 없이 스프링부트가 알아서 만들어준다.
 // Service와 Component의 차이는 '가독성'이다. 하지만 서비스에 해당하는 클래스는 Service를 사용하는 것이 좋다.
 @Service
+@RequiredArgsConstructor
 public class WiseSayingService {
-    private final List<WiseSaying> wiseSayings;
-    private long lastId;
-
-    public WiseSayingService() {
-        this.lastId = 0;
-        this.wiseSayings = new ArrayList<>() {{
-            add(
-                    WiseSaying
-                            .builder()
-                            .id(++lastId)
-                            .content("삶이 있는 한 희망은 있다")
-                            .author("키케로")
-                            .build()
-            );
-
-            add(
-                    WiseSaying
-                            .builder()
-                            .id(++lastId)
-                            .content("삶이 있는 한 희망은 있다")
-                            .author("키케로")
-                            .build()
-            );
-
-        }};
-    }
+    private final WiseSayingRepository wiseSayingRepository;
 
     public List<WiseSaying> findAll() {
-
-        return wiseSayings
-                .stream()
-                .sorted(Comparator.comparing(WiseSaying::getId).reversed())
-                .toList();
+        return wiseSayingRepository.findAll();
     }
 
     public WiseSaying write(String content, String author) {
         WiseSaying wiseSaying = WiseSaying
                 .builder()
-                .id(++lastId)
                 .content(content)
                 .author(author)
                 .build();
-
-        wiseSayings.add(wiseSaying);
-
+        wiseSayingRepository.save(wiseSaying);
         return wiseSaying;
     }
 
     public Optional<WiseSaying> findById(long id) {
-        return wiseSayings
-                .stream()
-                .filter(wiseSaying -> wiseSaying.getId() == id)
-                .findFirst();
+        return wiseSayingRepository.findById(id);
     }
 
     public boolean deleteById(long id) {
-        return wiseSayings.removeIf(wiseSaying -> wiseSaying.getId() == id);
+        return wiseSayingRepository.deleteById(id);
     }
 }
