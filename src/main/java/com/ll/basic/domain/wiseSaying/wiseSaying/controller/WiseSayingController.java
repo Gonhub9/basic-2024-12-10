271bdc550dcf927301fbc0670e74f7ffd8ab89c1
Gonhub9 <ho_gok@naver.com>
2024-12-11 14:48:13 +0900

    write ver.1

--- src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -4,22 +4,31 @@ import com.ll.basic.domain.wiseSaying.wiseSaying.entity.WiseSaying;
 import com.ll.basic.domain.wiseSaying.wiseSaying.service.WiseSayingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController// 여기에 붙이면 모든 메서드에 적용
+@RestController // 여기에 붙이면 모든 메서드에 적용
 @RequiredArgsConstructor // 생성자를 자동으로 만들어줌, final 붙은 필드를 자동으로 초기화해주는 어노테이션
-
 public class WiseSayingController {
     private final WiseSayingService wiseSayingService;
 
     // @Autowired로 필요한 것이 있으면 () 안에 적어라. 근데 생략 가능
     // @Autowired
-
     @GetMapping("/wiseSayings")
     public List<WiseSaying> getItems() {
         return wiseSayingService.findAll();
     }
 
+    @GetMapping("/wiseSayings/write")
+    public WiseSaying write(
+            // ? 뒤에 content 파라미터 값이 들어온다.
+            @RequestParam(name = "content") String content,
+
+            // ? 뒤에 author 파라미터 값이 들어온다.
+            @RequestParam(name = "author") String author
+    ) {
+        return wiseSayingService.write(content, author);
+    }
 }
--- src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/entity/WiseSaying.java
@@ -9,5 +9,4 @@ public class WiseSaying {
     private long id;
     private String content;
     private String author;
-
 }
